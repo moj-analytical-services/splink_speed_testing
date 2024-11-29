bd72125c0c6180308b6ae19469185599c6c27092
Robin Linacre <robinlinacre@hotmail.com>
2024-11-29 07:37:26 +0000

    works

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -1,8 +1,6 @@
 import tempfile
 from pathlib import Path
 import pytest
-import duckdb
-from splink_speed_testing.duckdb_backend import create_table_from_parquet
 
 from splink_speed_testing.create_tables import (
     create_comparison_test_table_full_name_most_nonmatching,
@@ -18,14 +16,3 @@ def comparison_test_data_path():
     )
 
     yield output_path
-
-
-@pytest.fixture(scope="session")
-def fullname_nonmatching_con(comparison_test_data_path):
-    con = duckdb.connect()
-    create_table_from_parquet(
-        con=con,
-        path=comparison_test_data_path,
-        table_name="fullname_nonmatching",
-    )
-    yield con
